5c0b0d7a57b088af8de53b1ae6a4264bf8607e42
TarieDume <tariedume@gmail.com>
2023-04-25 11:49:53 -0700

    aux

--- aux_error2.c
+++ aux_error2.c
@@ -12,8 +12,8 @@ char *ver_str;
 char *msg;
 ver_str = aux_itoa(datash->counter);
 msg = ": Unable to add/remove from environment\n";
-length = _strlen(datash->av[0]) + _strlen(ver_str);
-length += _strlen(datash->args[0]) + _strlen(msg) + 4;
+length = strlen(datash->av[0]) + strlen(ver_str);
+length += strlen(datash->args[0]) + strlen(msg) + 4;
 error = malloc(sizeof(char) * (length + 1));
 if (error == 0)
 {
@@ -21,13 +21,13 @@ free(error);
 free(ver_str);
 return (NULL);
 }
-_strcpy(error, datash->av[0]);
-_strcat(error, ": ");
-_strcat(error, ver_str);
-_strcat(error, ": ");
-_strcat(error, datash->args[0]);
-_strcat(error, msg);
-_strcat(error, "\0");
+strcpy(error, datash->av[0]);
+strcat(error, ": ");
+strcat(error, ver_str);
+strcat(error, ": ");
+strcat(error, datash->args[0]);
+strcat(error, msg);
+strcat(error, "\0");
 free(ver_str);
 return (error);
 }
@@ -43,8 +43,8 @@ int length;
 char *ver_str;
 char *error;
 ver_str = aux_itoa(datash->counter);
-length = _strlen(datash->av[0]) + _strlen(ver_str);
-length += _strlen(datash->args[0]) + 24;
+length = strlen(datash->av[0]) + strlen(ver_str);
+length += strlen(datash->args[0]) + 24;
 error = malloc(sizeof(char) * (length + 1));
 if (error == 0)
 {
@@ -52,13 +52,13 @@ free(error);
 free(ver_str);
 return (NULL);
 }
-_strcpy(error, datash->av[0]);
-_strcat(error, ": ");
-_strcat(error, ver_str);
-_strcat(error, ": ");
-_strcat(error, datash->args[0]);
-_strcat(error, ": Permission denied\n");
-_strcat(error, "\0");
+strcpy(error, datash->av[0]);
+strcat(error, ": ");
+strcat(error, ver_str);
+strcat(error, ": ");
+strcat(error, datash->args[0]);
+strcat(error, ": Permission denied\n");
+strcat(error, "\0");
 free(ver_str);
 return (error);
 }
--- aux_str2.c
+++ aux_str2.c
@@ -8,11 +8,11 @@ char *_strdup(const char *s)
 {
 char *new;
 size_t len;
-len = _strlen(s);
+len = strlen(s);
 new = malloc(sizeof(char) * (len + 1));
 if (new == NULL)
 return (NULL);
-_memcpy(new, s, len + 1);
+memcpy(new, s, len + 1);
 return (new);
 }
 /**
